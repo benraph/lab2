f2d2b59f25701b1671c3c25c155735e296733d18
Ben Raphael <benraph02@gmail.com>
2023-09-25 10:59:05 -0500

    added button and an EditText field that when the button is clicked it sends a message to the Logcat "button clicked" and Toasts the text in the EditText field

--- app/src/main/java/com/cs407/lab2/MainActivity.java
+++ app/src/main/java/com/cs407/lab2/MainActivity.java
@@ -2,6 +2,10 @@ package com.cs407.lab2;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //hello
+    }
+
+    public void clickFunction(View view){
+        EditText myTextField = (EditText) findViewById(R.id.myTextField);
+        Log.i("INFO","Button Clicked");
+
+        Toast.makeText(MainActivity.this, myTextField.getText().toString(),Toast.LENGTH_LONG).show();
     }
 }
